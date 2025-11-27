c80a2d769f94a033c355b42e7e5cacd48e2d0d34
TomaNeshkovSoftUni <toma.neshkov.highschool@buditel.bg>
2025-11-27 08:55:04 +0200

    Initial commit

---
+++ Student System/Student System/Data/StudentSystemDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_System.Data
+{
+    public class StudentSystemDbContext : DbContext
+    {
+
+    }
+}
