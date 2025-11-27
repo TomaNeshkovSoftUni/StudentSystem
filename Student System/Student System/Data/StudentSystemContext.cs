2d4db905b8879c1b8ca0617c721b658329ca9f0f
TomaNeshkovSoftUni <toma.neshkov.highschool@buditel.bg>
2025-11-27 09:51:22 +0200

    Update StudentSystemContext.cs

--- Student System/Student System/Data/StudentSystemContext.cs
+++ Student System/Student System/Data/StudentSystemContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Student_System.Models;
+using StudentSystem;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,20 @@ namespace Student_System.Data
 {
     public class StudentSystemContext : DbContext
     {
+        StudentSystemContext(DbContextOptions<StudentSystemContext> options)
+            : base(options)
+        {
+        }
 
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<HomeworkSubmissions> Homeworks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
     }
 }
